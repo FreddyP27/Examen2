d789890abe082c1b6448aea8b186ae33aecda91c
FreddyP27 <freddyperezc27+1@gmail.com>
2025-03-20 16:08:37 +0100

    commit

---
+++ src/main/java/examen2/freddyarmando/perezcampos/examen/Main.java
@@ -0,0 +1,24 @@
+package examen2.freddyarmando.perezcampos.examen;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+	private static final Logger logger = LoggerFactory.getLogger(Main.class);
+	public static void main(String[] args) {
+		
+			System.out.println("Hola Mundo2");
+			System.out.println("Buenas tardes");
+			System.out.println("Prueba de modificación");
+			System.out.println("Prueba desde GitHub");
+			
+//			logger.debug("Hola mundo debug");
+			
+			 logger.debug("Debug Hello world2!");
+		     logger.info("Info Hello world2!");
+		     logger.warn("Warn Hello world2!");
+		     logger.error("Error Hello world2!");
+		
+	}
+
+}
